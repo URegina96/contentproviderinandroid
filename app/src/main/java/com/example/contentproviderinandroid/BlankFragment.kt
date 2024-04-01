bc84ab4ced6ea67856afadfb225c498ba33cc856
regina <usmanova.re@yandex.ru>
2024-04-01 23:24:40 +0500

    Настройка макетов
    Код позволяет получить данные из ContentProvider через ContentResolver и отображает информацию о книгах в логах. Для правильного отображения элементов в активити и фрагменте с помощью XML файлов, необходимо настроить макеты (layout) для соответствующих файлов (activity_main.xml и fragment_blank.xml)
    
    После этого нужно изменить код в MainActivity, чтобы взаимодействовать с элементами в макетах
    
    Это позволит отобразить информацию о книге в активити
    
    Чтобы аналогично взаимодействовать с элементами макета fragment_blank.xml, вносим следующие изменения в соответствующем фрагменте BlankFragment

--- app/src/main/java/com/example/contentproviderinandroid/BlankFragment.kt
+++ app/src/main/java/com/example/contentproviderinandroid/BlankFragment.kt
@@ -1,59 +1,41 @@
 package com.example.contentproviderinandroid
 
+import android.content.ContentResolver
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BlankFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_blank, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        val bookTitleTextView = view.findViewById<TextView>(R.id.bookTitleTextView)
+        val bookAuthorTextView = view.findViewById<TextView>(R.id.bookAuthorTextView)
+
+        val contentResolver: ContentResolver = requireContext().contentResolver
+        val uri: Uri = Uri.parse("content://com.example.bookscontentprovider/books")
+        val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+
+        cursor?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                val titleIndex = cursor.getColumnIndex("title")
+                val authorIndex = cursor.getColumnIndex("author")
+
+                val bookTitle = cursor.getString(titleIndex)
+                val bookAuthor = cursor.getString(authorIndex)
+
+                bookTitleTextView.text = bookTitle
+                bookAuthorTextView.text = bookAuthor
             }
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/example/contentproviderinandroid/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderinandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.ContentResolver
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,24 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Получение данных с помощью ContentResolver
         val contentResolver: ContentResolver = contentResolver
         val uri: Uri = Uri.parse("content://com.example.bookscontentprovider/books")
         val cursor = contentResolver.query(uri, null, null, null, null)
 
-        // Обработка полученных данных
         cursor?.let {
-            while (it.moveToNext()) {
+            if (it.moveToFirst()) {
                 val titleIndex = it.getColumnIndex("title")
                 val authorIndex = it.getColumnIndex("author")
 
-                if (titleIndex >= 0 && authorIndex >= 0) {
-                    val bookTitle = it.getString(titleIndex)
-                    val bookAuthor = it.getString(authorIndex)
-                    Log.d("Book Data", "Title: $bookTitle, Author: $bookAuthor")
-                } else {
-                    Log.d("Book Data", "Invalid column name")
-                }
+                val bookTitle = it.getString(titleIndex)
+                val bookAuthor = it.getString(authorIndex)
+
+                val bookTitleTextView = findViewById<TextView>(R.id.bookTitleTextView)
+                val bookAuthorTextView = findViewById<TextView>(R.id.bookAuthorTextView)
+
+                bookTitleTextView.text = bookTitle
+                bookAuthorTextView.text = bookAuthor
             }
             it.close()
         }
